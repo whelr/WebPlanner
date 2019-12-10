16c6d4d99559370e2c235e14d7a47e8c324f8408
Josh <jousua.giurculete@wsu.edu>
2019-12-10 15:44:02 -0800

    backup

--- app/imports/startup/server/accounts.js
+++ app/imports/startup/server/accounts.js
@@ -3,14 +3,15 @@ import { Accounts } from 'meteor/accounts-base';
 
 /* eslint-disable no-console */
 
-/* When running app for first time, pass a settings file to set up a default user account. */
-if (Meteor.users.find().count() === 0) {
-  if (Meteor.settings.defaultAccount) {
+Template.Signup_Page.events({
+  'submit form': function(event){
+    event.preventDefault();
+    var email = $('[name=email]').val();
+    var password = $('[name=password]').val();
     Accounts.createUser({
-      username: Meteor.settings.defaultAccount.username,
-      password: Meteor.settings.defaultAccount.password,
+      email: email,
+      password: password
     });
-  } else {
-    console.log('No default user!  Please invoke meteor with a settings file.');
+    Router.go('home');
   }
-}
+});
--- app/imports/ui/pages/login.js
+++ app/imports/ui/pages/login.js
@@ -1,2 +1,13 @@
 import { Template } from 'meteor/templating';
 import { Stuff } from '../../api/stuff/stuff.js';
+
+Template.Login_Page.events({
+  'submit form': function(event){
+    event.preventDefault();
+    var email = $('[name=email]').val();
+    var password = $('[name=password]').val();
+    Meteor.loginWithPassword(email, password, function(error){
+      console.log(error.reason);
+    });
+  }
+});
