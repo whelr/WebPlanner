86318386bcc6845467bd81d40c8449fe10e4d982
Michael Wheeler <michael.j.wheeler@wsu.edu>
2019-12-11 21:16:38 -0800

    oh hey, we got it again

--- app/imports/ui/pages/signup.js
+++ app/imports/ui/pages/signup.js
@@ -1,2 +1,22 @@
 import { Template } from 'meteor/templating';
 import { Stuff } from '../../api/stuff/stuff.js';
+
+Template.Signup_Page.events({
+  'submit form': function(event){
+    event.preventDefault();
+    const email = $('[name=email]').val();
+    const password = $('[name=password]').val();
+    Accounts.createUser({
+      email: email,
+      password: password
+    });
+    FlowRouter.go('Login_Page');
+  }
+});
+
+FlowRouter.route('/signup', {
+  name: 'Signup_Page',
+  action() {
+    Accounts.createUser(username, password);
+  },
+})
